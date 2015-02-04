7e21ef982ee0ef146b4eabe22a090057a1699aa9
tigeryu <xiaojie.yu@yahoo.com>
2015-02-04 15:35:43 -0800

    changed the s+=1 this works but s=s+1 is not working , have error

---
+++ src/polymorphism/Bmw.java
@@ -0,0 +1,11 @@
+package polymorphism;
+
+/**
+ * Created by v-daisyq on 2/1/2015.
+ */
+public class Bmw extends Car{
+
+        void run(){
+            System.out.println("the Bmw speed is 130 mile per hour");
+        }
+}
