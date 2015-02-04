5a9d8d7136572ab190526f11e299d4464e6da62d
tigeryu <xiaojie.yu@yahoo.com>
2015-02-04 12:28:38 -0800

    first upload

---
+++ src/org/waspec/class001/UploadTest.java
@@ -0,0 +1,12 @@
+package org.waspec.class001;
+
+/**
+ * Created by v-daisyq on 2/4/2015.
+ */
+public class UploadTest {
+
+    public static void main(String[]args){
+        System.out.println("This is a test for the first upload to github");
+    }
+
+}
