c36cb24ca4b62278ecb8c201704e300979800f16
tigeryu <xiaojie.yu@yahoo.com>
2015-02-05 00:08:06 -0800

    class9 statements

---
+++ src/org/waspec/class001/Class9.java
@@ -0,0 +1,33 @@
+package org.waspec.class001;
+
+/**
+ * Created by v-daisyq on 2/4/2015.
+ */
+public class Class9 {
+    //数据类型提升  ， 两个数据进行操作的时候将提高到精度高的类型   {{}} 块当作一条语句 statement,s
+
+    public static void main(String[] args) {
+
+        final int x = 100;    //只有一次赋值机会，之后无法改变这个值；
+        System.out.println(x);
+        Teve teve=new Teve();
+        teve.play();
+        Cal.add(12, 16);   //side effect;        12,16   在调用方法的时候所提供的值和表达式   是实际参数
+
+    }
+}
+
+    class Teve {
+        public void play() {
+            System.out.println("CCTV");
+        }
+    }
+
+    class Cal {
+        public static double add(double a, double b) {     // double a, double b; 在声明定义方法的时候声明的参数变量 形式参数
+            double total = a + b;
+            System.out.println(total);
+            return total;
+        }
+    }
+
