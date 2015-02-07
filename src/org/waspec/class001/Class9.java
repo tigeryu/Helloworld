50e565cea1c5da6d33ed0276f4d53169f740e10c
tigeryu <xiaojie.yu@yahoo.com>
2015-02-06 16:06:23 -0800

    class9 record

--- src/org/waspec/class001/Class9.java
+++ src/org/waspec/class001/Class9.java
@@ -6,6 +6,8 @@ package org.waspec.class001;
 public class Class9 {
     //数据类型提升  ， 两个数据进行操作的时候将提高到精度高的类型   {{}} 块当作一条语句 statement,s
 
+   public static int a;
+
     public static void main(String[] args) {
 
         final int x = 100;    //只有一次赋值机会，之后无法改变这个值；
@@ -13,11 +15,18 @@ public class Class9 {
         Teve teve=new Teve();
         teve.play();
         Cal.add(12, 16);   //side effect;        12,16   在调用方法的时候所提供的值和表达式   是实际参数
+        System.out.println(a);
+        System.out.println(teve.x);
+
+
+
+
 
     }
 }
 
     class Teve {
+        int x;
         public void play() {
             System.out.println("CCTV");
         }
